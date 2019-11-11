ff3e05fe57ac3e22ea1616dc67aa091acb60a4eb
lcyhenshuai <57220552+lcyhenshuai@users.noreply.github.com>
2019-11-11 19:56:30 +0800

    生成随机50道算式题
    1.java语言环境完成50道100以内的加减法口算习题程序编写，算式产生功能；题目生成功能；加减法计算功能；我写了50道随机算式，数组a[i]存的是加数a，数组b[i]存的是加数b,sum数组存的50道计算的结果，yuSuan数组存的是50个随机的字符，里面是随机的加法，减法。

--- ChuTiOne.java
+++ ChuTiOne.java
@@ -1,19 +1,19 @@
 import java.util.Random;
 
 
-public class Chuti1 {
+public class ChuTiOne {
 
 	public static void main(String[] args) {
-		int a[]=new int[50];
-		int b[]=new int[50];
-		int sum[]=new int [50];
-		String yuSuan[]=new String[50];
+		int a[]=new int[50];//存放第一个数的数组
+		int b[]=new int[50];//存放第二个数的数组
+		int sum[]=new int [50];//存放运算结果的数组
+		String yuSuan[]=new String[50];//存放运算符的数组
 		Random r=new Random();
 		for(int i=0;i<50;i++){
 			a[i]=r.nextInt(100);
 			b[i]=r.nextInt(100);
 			boolean ziFu;
-			ziFu=r.nextBoolean();
+			ziFu=r.nextBoolean();//随机生成运算符
 			if(ziFu){yuSuan[i]="+";sum[i]=a[i]+b[i];}
 			else {yuSuan[i]="-";sum[i]=a[i]-b[i];}
 			System.out.println(a[i]+yuSuan[i]+b[i]);
