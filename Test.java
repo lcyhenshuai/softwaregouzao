04fc2b94b051ef7d9ef598110dd8e76103d30d6b
lcyhenshuai <57220552+lcyhenshuai@users.noreply.github.com>
2019-11-11 20:04:59 +0800

    迭代4，Test为专门运行ChuTiFour的类
    (4)对之前的需求进行模块化管理，即a数组存放50个加数，b数组存放50个加数，sum数组存放50个运算的结果，yuSuan字符串数组产生50个运算符，shengChengFivtySuanShi（）是生成50道随机算式的方法，shuChu()为输出算式的方法，jianYan()为检验结果是否符合要求方法，ShengChengSuanShi(int j)是生成每一个算式的方法。ChuTiFour为存放这些方法的类，Test为main方法执行的类。

---
+++ Test.java
@@ -0,0 +1,17 @@
+package Chuti1;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+	
+		ChuTiFour chuti=new ChuTiFour();
+		chuti.shengChengFivtySuanShi();
+		chuti.jianYan();
+	     chuti.shuChu();
+        
+      
+     
+	}
+
+}
