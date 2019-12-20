33cdb2e1ad6f3979344dcbab6988ede8329124df
lcyhenshuai <57220552+lcyhenshuai@users.noreply.github.com>
2019-12-20 10:30:20 +0800

    软件构造GUI设计实验四
    我是用swing开发工具做的，ChuTi.form时设计的jframe,judge.form时模式对话框，是当你点完导入习题文件，点完导出习题文件后，点击批改按钮后，弹出Judge.form显示你做对了几道题，做错了几道题，总共有几道题，Helpter是窗体间传值的工具类。

---
+++ Helpter.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Helpter {
+   static int tiShu;
+  static int correct;
+  static int error;
+ public static void main(String[] args) {
+	System.out.println(tiShu);
+	System.out.println(correct);
+	System.out.println(error);
+}
+}
